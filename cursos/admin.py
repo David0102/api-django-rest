d1efcf583bf02702f7df2eff4273a00dc52a5830
David0102 <davidmoura0102@gmail.com>
2024-03-05 17:24:09 -0300

    create serializers Avaliacao and Curso

--- cursos/admin.py
+++ cursos/admin.py
@@ -4,7 +4,9 @@ from .models import Curso, Avaliacao
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     list_display = ["titulo", "url", "criado", "modificado", "ativo"]
+    search_fields = ["titulo", "descricao"]
 
 @admin.register(Avaliacao)
 class AvaliacaoAdmin(admin.ModelAdmin):
     list_display = ["curso", "nome", "email", "avaliacao", "criado", "modificado", "ativo"]
+    search_fields = ["curso", "nome", "email", "avaliacao"]
