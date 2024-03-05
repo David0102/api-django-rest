2c453f2808df28b7c60e859ea6132e939e6078ac
David0102 <davidmoura0102@gmail.com>
2024-03-05 15:30:23 -0300

    create models Curso and Avaliacao

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Curso, Avaliacao
 
-# Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ["titulo", "url", "criado", "modificado", "ativo"]
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ["curso", "nome", "email", "avaliacao", "criado", "modificado", "ativo"]
